64a449fbf0c1612643d60fda20c02e94672b6deb
xerjek <jekxer.silao@gmail.com>
2023-09-19 18:34:51 +0800

    try

--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-import Button from "./Button";
-import Menu from "./Menu";
-import SocialMediaIcon from "./SocialMediaIcon";
-import Card from "./Card";
-import LabelInput from "./LabelInput";
-import Reveal from "./Reveal";
-
-export { Button, Menu, SocialMediaIcon, Card, LabelInput, Reveal };
--- src/views/index.ts
+++
@@ -1,6 +0,0 @@
-import Hero from './Hero';
-import About from './About';
-import Projects from './Projects';
-import Contact from './Contact';
-
-export { Hero, About, Projects, Contact };
